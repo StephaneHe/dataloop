c60b7a7faea313167fef6178f016f4d8278c6358
Stephane Hercot <stephane.hercot@gmail.com>
2023-10-15 18:25:09 +0200

    gke/main.tf

--- clients/client1/gcp/production/modules/gke/main.tf
+++ clients/client1/gcp/production/modules/gke/main.tf
@@ -1,12 +1,67 @@
 resource "google_container_cluster" "primary" {
-  name     = "my-gke-cluster"
-  location = var.gcp_region
+  name               = "my-gke-cluster"
+  location           = var.gcp_region
+  initial_node_count = 1
 
-  # ... autres configurations pour le cluster ...
+  master_auth {
+    username = ""
+    password = ""
 
-  node_pool {
-    # ... configurations pour le node pool ...
+    client_certificate_config {
+      issue_client_certificate = false
+    }
   }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+    ]
+
+    labels = {
+      environment = "production"
+    }
+
+    tags = ["gke-node"]
+  }
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "my-node-pool"
+  location   = var.gcp_region
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
+
+  node_config {
+    preemptible  = false
+    machine_type = "n1-standard-1"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+    ]
+  }
+}
+
+provider "kubernetes" {
+  host                   = google_container_cluster.primary.endpoint
+  token                  = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  client_certificate     = base64decode(google_container_cluster.primary.master_auth.0.client_certificate)
+  client_key             = base64decode(google_container_cluster.primary.master_auth.0.client_key)
 }
 
 resource "kubernetes_namespace" "services" {
